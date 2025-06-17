3dd35f6efa46e177cd0ef17ad9d02e21bb76ac5b
tyoshilab <aiko.jitsui@gmail.com>
2025-06-17 16:41:22 -0700

    update: add test code

--- Part1.py
+++ Part1.py
@@ -3,6 +3,9 @@ def main():
     n = int(input("digit?: "))
     nthdigit(number, n)
 
+    # test(234455.34)
+    # test(123456)
+
 
 # main logic
 def nthdigit(number: float, n: int) -> int:
@@ -13,6 +16,32 @@ def nthdigit(number: float, n: int) -> int:
     q = r // 10 ** (n - 1)
     print(int(q))
     return int(q)
+
+
+# testcode
+def test(number: float):
+    reversed_number = str(number)[::-1]
+    if isinstance(number, float):
+        fractional_part, int_part = reversed_number.split(".")
+        fractional_list, int_list = list(fractional_part[::-1]), list(int_part)
+
+        for i in range(1, len(fractional_list) + 1):
+            ans = fractional_list[i - 1]
+            print(
+                "nthdigit({}, {}) == {}: {}".format(
+                    number, -i, ans, nthdigit(number, -i) == int(ans)
+                )
+            )
+    else:
+        int_list = list(reversed_number)
+
+    for i in range(1, len(int_list) + 1):
+        ans = int_list[i - 1]
+        print(
+            "nthdigit({}, {}) == {}: {}".format(
+                number, i, ans, nthdigit(number, i) == int(ans)
+            )
+        )
 
 
 if __name__ == "__main__":
